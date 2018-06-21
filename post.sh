a614f5836dfed885815181037627f6d471c899eb
Gokul Wimal <kokul@iastate.edu>
2018-06-21 08:50:04 -0500

    added python and r packages to test

--- post.sh
+++ post.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 apt-get -q -y update
-apt-get -yq install less vim wget python-pip libfuse2 r-base openjdk-8-jdk  debconf-utils
+apt-get -q -y install debconf-utils less vim wget python-pip libfuse2 r-base openjdk-8-jdk
+
+# R -e 'install.packages(c("data.table","futile.logger","ggplot2","ontologyIndex","reshape2","scales","jsonlite","yaml"))'
+sed -i 's/make/make -j4/g' /usr/lib/R/etc/Renviron
+R -e 'install.packages(c("data.table","futile.logger","ontologyIndex","scales","yaml"))'
+
+pip install -r /root/requirements.txt
