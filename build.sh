cafd4e26ed4faac06f7bf3d13312199a426f00e2
Kokulapalan Wimalanathan <kokul@iastate.edu>
2019-09-22 23:04:39 -0500

    added awscli

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 instance_name="GOMAP-base"
 sudo rm -r $instance_name*
-sudo singularity build -i $instance_name.simg Singularity.xenial
+sudo singularity build --tmpdir tmp $instance_name.simg Singularity.bionic
